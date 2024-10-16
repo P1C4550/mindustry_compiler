46577936c398ee1435f7d07bc1bb02c6bd8d2a86
pawel <pawel.herok@gmail.com>
2024-10-16 18:25:49 +0200

    Read command line arguments

--- main.c
+++ main.c
@@ -2,7 +2,11 @@
 
 int main(int argc, char *argv[]) { 
 
-    printf("%d", argc);
-
+    printf("%d\n", argc);
+    for (int arg_n = 0; arg_n <= argc; ++arg_n)
+    {
+        // argv[arg_n] is a pointer to a string
+        printf("%s\n", argv[arg_n]);
+    }
 	return 0;
 }
